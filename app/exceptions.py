afeb3183d19d0ac4fff12bb29ed94d417ff0c103
I-am-told <artyomkolesnikov1990@gmail.com>
2025-01-10 12:51:50 +0200

    add add bookings

--- app/exceptions.py
+++ app/exceptions.py
@@ -36,3 +36,8 @@ class IncorrectTokenFormatException(BookingException):
     
 class UserIsNotExistException(BookingException):
     status_code = status.HTTP_401_UNAUTHORIZED
+    
+    
+class RoomCannotBeBookedException(BookingException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = 'Не осталось свободных номеров'
